2c038b7844d5cee1a1eccd9e56a13134646511f5
dbarriga <david.barriga@bigcommerce.com>
2019-03-28 12:31:21 -0600

    component creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { 
+  MatToolbarModule, 
+  MatButtonModule 
+} from '@angular/material';
+
+
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { TeamComponent } from './team/team.component';
+import { PlayerComponent } from './player/player.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    TeamComponent,
+    PlayerComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
